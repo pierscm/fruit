428f3fe82827d9e6f4a017862b6a6292c3d63f7c
Cassandra Pierson <piersoncassandra@gmail.com>
2020-08-17 23:25:06 -0500

    Fruit item componenet design.

---
+++ client/src/components/FruitItem.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-card class="mx-auto" max-width="400">
+    <v-img
+      height="200px"
+      src="https://cosmosmagazine.com/wp-content/uploads/2020/02/100118_Debunked_01.jpg"
+    >
+    </v-img>
+    <v-card-title>Banana</v-card-title>
+
+    <v-card-text class="text--primary">
+      <div class="text-left">13 oz</div>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "FruitItem"
+};
+</script>
--- client/src/components/FruitList.vue
+++ client/src/components/FruitList.vue
@@ -1,15 +1,22 @@
 <template>
   <v-container>
     <v-row class="text-center">
-      <v-col class="mb-4"> </v-col>
+      <v-col class="mb-4">
+        <FruitItem />
+      </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script>
+import FruitItem from "../components/FruitItem";
+
 export default {
   name: "Fruit",
 
-  data: () => ({})
+  data: () => ({}),
+  components: {
+    FruitItem
+  }
 };
 </script>
